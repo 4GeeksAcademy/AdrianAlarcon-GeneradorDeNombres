9cf1dd120c78b9fe031453827c8bdffb02798390
COCOASOO <85440064+COCOASOO@users.noreply.github.com>
2024-03-23 10:10:23 +0000

    app adaptado

--- src/app.js
+++ src/app.js
@@ -6,6 +6,24 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  function randomGenerator(arr1, arr2, arr3) {
+    let dominios = [];
+    for (let i = 0; i < arr1.length; i++) {
+      for (let j = 0; j < arr2.length; j++) {
+        for (let k = 0; k < arr3.length; k++) {
+          dominios.push(arr1[i] + arr2[j] + arr3[k] + ".com");
+        }
+      }
+    }
+    return dominios;
+  }
+  let dominiosGen = randomGenerator(pronoun, adj, noun);
+
+  for (let index of dominiosGen) {
+    console.log(index);
+  }
 };
